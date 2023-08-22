d4b7bebb51ee470905baf02a55f44fe4e0f1beb8
bhanuchennuri <119839008+bhanuchennuri@users.noreply.github.com>
2023-08-22 15:18:14 +0530

    Add files via upload

--- class.cpp
+++ class.cpp
@@ -1,6 +1,28 @@
+#include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-void Takeinput()
+
+class exam{
+	public:
+		string name,rollno;
+		int marks;
+		float perc;
+		exam(string rollno,string name,int marks,float perc)
+		{
+		this->rollno=rollno;
+		this->name=name;
+		this->marks=marks;
+		this->perc=perc;
+		}
+
+		void display()
+		{
+			cout<<rollno<<""<<name<<" "<<marks<<" "<<perc<<" "<<endl;
+		}
+	};
+int main()
 {
+	exam s1("BHANU","22A91A1296",567,98.96);
+	s1.display();
 	
 }
